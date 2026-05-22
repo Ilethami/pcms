e97653478d2e477fc0470dd08c51a96fec4d7429
jewel <frcs5781@gmail.com>
2026-05-22 16:16:39 +0800

    Link test

--- src/Components/Media.jsx
+++ src/Components/Media.jsx
@@ -63,9 +63,8 @@ export default function Hero() {
       </div>
       <div className="flex flex-row gap-6">
         <div className="text-[#4E9372] text-center font-poppins text-[20px] font-normal leading-[40px]" >
-          <a href="figma.com/proto/m5EIX6lGYF0GMlKl4O5aSP/Pickleball-Court-Management-System?node-id=705-1404&t=p642Rj7ORh4BDZQ2-0&scaling=min-zoom&content-scaling=fixed&page-id=534%3A6407&starting-point-node-id=705%3A1404&show-proto-sidebar=1&fuid=1230855733563728487 " target="_blank" rel="noopener noreferrer">
-          Phone
-          </a>
+          <a href="https://www.figma.com/proto/m5EIX6lGYF0GMlKl4O5aSP/Pickleball-Court-Management-System?node-id=534-6408&t=mYWQf4QjZ6RX9Xgt-1&scaling=min-zoom&content-scaling=fixed&page-id=534%3A6407&starting-point-node-id=534%3A6408">
+          Phone</a>
         </div>
         <div className="text-[#4E9372] text-center font-poppins text-[20px] font-normal leading-[40px]" >
           Desktop
